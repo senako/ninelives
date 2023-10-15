0b7fe5df112cb2b3e02331c29fbe0c2e79bf1819
Sena Korkaya <skorkaya6@gatech.edu>
2023-10-14 22:50:43 -0400

    please

--- ninelives/core/src/com/mygdx/game/MyGame.java
+++ ninelives/core/src/com/mygdx/game/MyGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -15,68 +16,30 @@ import sun.java2d.pipe.SpanShapeRenderer;
 
 public class MyGame extends Game {
 	SpriteBatch batch;
-	Texture backgroundImg;
-	Texture buttonImg;
 	Stage stage;
 	private Game game;
-	float inputX;
-	float inputY;
-
 	Screen nextScreen;
 
-	SimpleButton button;
-
-	public MyGame() {
-		game = this;
-	}
+	OrthographicCamera camera;
 
 	@Override
 	public void create () {
-		stage = new Stage(new ScreenViewport());
 		batch = new SpriteBatch();
-		Gdx.input.setInputProcessor(stage);
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-		backgroundImg = new Texture("startScreen.png");
-		buttonImg = new Texture("startButton.png");
-		nextScreen = new FirstPuzzleScreen(game);
-
-		button = new SimpleButton(buttonImg, 400, 400, 200, 80);
-		stage.addActor(button);
+		setScreen(new StartScreen(this));
 	}
 
 	@Override
 	public void render () {
 
 		super.render();
-		stage.act(Gdx.graphics.getDeltaTime());
-
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		batch.begin();
-		batch.draw(backgroundImg, 20, 20);
-		button.draw(batch);
-		batch.end();
-
-		stage.draw();
-
-
-		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-			inputX = Gdx.input.getX();
-			inputY = Gdx.input.getY();
-			button.update(batch, inputX, inputY, game, nextScreen);
-			dispose();
-			super.setScreen(nextScreen);
-
-		}
 
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		backgroundImg.dispose();
-		buttonImg.dispose();
-		stage.dispose();
 	}
 }
